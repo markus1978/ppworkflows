285bfa8c4ab4e32bec822e7dfee06370c6e2e26d
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-03-05 13:32:23 +0100

    Release.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ppworkflows',
-      version='0.1.1',
+      version='0.1.2',
       description='Execute data-driven task-based workflows in parallel with Python.',
       url='http://github.com/markus1978/ppworkflows',
       author='Markus Scheidgen',
